e4eccb36ffbead5665c131fd2451c08a8705afab
Jade Meskill <jade.meskill@gmail.com>
2017-05-17 13:31:09 -0700

    v11.3.6 build again

--- deploy/windows/deploy.sh
+++ deploy/windows/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main() {
-	yarn run package-win-x64 -- --publish onTag || exit 1
-	yarn run package-win-ia32 -- --publish onTag || exit 1
+	yarn run package-win-x64 -- --publish onTagOrDraft || exit 1
+	yarn run package-win-ia32 -- --publish onTagOrDraft || exit 1
 }
 
 main "$@"
